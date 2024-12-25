7b47dcb75ddd5e55e4c1979e7391a25ea38990e5
Tonykohh <okohtony2000@gmail.com>
2024-12-25 01:32:34 +0100

    new commmit

---
+++ src/test/java/Setup.java
@@ -0,0 +1,12 @@
+import io.appium.java_client.android.AndroidDriver;
+import io.appium.java_client.ios.IOSDriver;
+
+public class Setup {
+
+   public  AndroidDriver driver;
+   public IOSDriver iosDriver;
+
+
+
+
+}
