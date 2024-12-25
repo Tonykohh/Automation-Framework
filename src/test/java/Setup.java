8ef4c7ea2ccc54acaab1205f0a4e203208bdc01d
Tonykohh <okohtony2000@gmail.com>
2024-12-25 20:31:37 +0100

    first framework push

--- src/test/java/Setup.java
+++ src/test/java/Setup.java
@@ -1,12 +1,39 @@
 import io.appium.java_client.android.AndroidDriver;
 import io.appium.java_client.ios.IOSDriver;
+import org.openqa.selenium.remote.DesiredCapabilities;
+
+import java.net.MalformedURLException;
+import java.net.URL;
 
 public class Setup {
 
-   public  AndroidDriver driver;
-   public IOSDriver iosDriver;
 
+  public static DesiredCapabilities caps = new DesiredCapabilities();
+    public static AndroidDriver driver;
 
+   public static void capabilities (){
 
+      caps.setCapability("platformName", "android");
+      caps.setCapability("automationName", "UIAutomator2");
+      caps.setCapability("deviceName","TECNO-KJ5");
+      caps.setCapability("appPackage", "com.gluak.f24");
+      caps.setCapability("appActivity", "com.gluak.f24.GluakLibs.ui.container.AppMenu");
+      caps.setCapability("noReset", true);
+   }
 
+   public static void mainSetup () throws MalformedURLException {
+      capabilities();
+       try {
+           driver = new AndroidDriver(new URL("http://127.0.0.1:4723"), caps);
+           System.out.println("Session started successfully!");
+       } catch (Exception e) {
+           e.printStackTrace();
+       }
+
+   }
+
+   public static void main(String[] args) throws MalformedURLException {
+       mainSetup();
+   }
 }
+
